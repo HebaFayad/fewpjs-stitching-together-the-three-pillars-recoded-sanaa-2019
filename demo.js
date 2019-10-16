70f0904795ab1f99ec28f20f46d0d6c97e8c157a
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-10-16 06:48:54 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -23,7 +23,7 @@ function likeCallback(e) {
       // STEP 2: Uncomment the next 3 lines.
       // We'll use Pillar 1 (DOM Manipulation) to update the screen and
       // mimic Pillar 3 (Server Communication) to only update the screen if the
-      // sending of information to the server succeeds.
+      ///sending of information to the server succeeds.
       alert("You notified the server!");
       // alert(serverMessage);
       // heart.innerText = glyphStates[heart.innerText];
